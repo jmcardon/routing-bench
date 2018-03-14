afac41b1eb207f500ca6261d4c8e20e700a5c34b
jose <cardona.josem@gmail.com>
2018-03-13 20:32:11 -0400

    ross was right and I a fool

--- src/main/scala/Bench/Bencher.scala
+++ src/main/scala/Bench/Bencher.scala
@@ -12,15 +12,13 @@ import org.http4s._
 class Bencher {
 
   @Benchmark
-  def firstAndLastMany: Array[IO[Response[IO]]] = {
-    Array(HelloWorldServer.CMany.run(Bencher.routes(0)),
-          HelloWorldServer.CMany.run(Bencher.routes(19)))
+  def lastMany: IO[Response[IO]] = {
+    HelloWorldServer.CMany.run(Bencher.routes(19))
   }
 
   @Benchmark
-  def firstAndLastTwo: Array[IO[Response[IO]]] = {
-    Array(HelloWorldServer.CTwo.run(Bencher.routes(0)),
-          HelloWorldServer.CTwo.run(Bencher.routes(19)))
+  def lastTwo: IO[Response[IO]] = {
+    HelloWorldServer.CTwo.run(Bencher.routes(19))
   }
 
   @Benchmark
@@ -34,43 +32,13 @@ class Bencher {
   }
 
   @Benchmark
-  def lastMany: IO[Response[IO]] = {
-    HelloWorldServer.CMany.run(Bencher.routes(19))
+  def middleTwo: IO[Response[IO]] = {
+    HelloWorldServer.CTwo.run(Bencher.routes(10))
   }
 
   @Benchmark
-  def lastTwo: IO[Response[IO]] = {
-    HelloWorldServer.CTwo.run(Bencher.routes(19))
-  }
-
-  @Benchmark
-  def randomOrderTwo: Array[IO[Response[IO]]] = {
-    Array(
-      HelloWorldServer.CTwo.run(Bencher.routes(19)),
-      HelloWorldServer.CTwo.run(Bencher.routes(5)),
-      HelloWorldServer.CTwo.run(Bencher.routes(6)),
-      HelloWorldServer.CTwo.run(Bencher.routes(10)),
-      HelloWorldServer.CTwo.run(Bencher.routes(1)),
-      HelloWorldServer.CTwo.run(Bencher.routes(2)),
-      HelloWorldServer.CTwo.run(Bencher.routes(12)),
-      HelloWorldServer.CTwo.run(Bencher.routes(18)),
-      HelloWorldServer.CTwo.run(Bencher.routes(13))
-    )
-  }
-
-  @Benchmark
-  def randomOrderMany: Array[IO[Response[IO]]] = {
-    Array(
-      HelloWorldServer.CMany.run(Bencher.routes(19)),
-      HelloWorldServer.CMany.run(Bencher.routes(5)),
-      HelloWorldServer.CMany.run(Bencher.routes(6)),
-      HelloWorldServer.CMany.run(Bencher.routes(10)),
-      HelloWorldServer.CMany.run(Bencher.routes(1)),
-      HelloWorldServer.CMany.run(Bencher.routes(2)),
-      HelloWorldServer.CMany.run(Bencher.routes(12)),
-      HelloWorldServer.CMany.run(Bencher.routes(18)),
-      HelloWorldServer.CMany.run(Bencher.routes(13))
-    )
+  def middleMany: IO[Response[IO]] = {
+    HelloWorldServer.CMany.run(Bencher.routes(10))
   }
 
 }
