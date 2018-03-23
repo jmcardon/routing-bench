ca5e4d1055cae86f7237a0af0e05e53bc7ca5f91
jose <cardona.josem@gmail.com>
2018-03-22 22:39:54 -0400

    dora

--- src/main/scala/Bench/MultipartBench.scala
+++ src/main/scala/Bench/MultipartBench.scala
@@ -18,7 +18,7 @@ class MultipartBench {
   import MultipartBench._
 
   @Benchmark
-  def smalloldMultipart: Multipart[IO] = {
+  def smallOldMultipart: Multipart[IO] = {
     encoderLive
       .decode(requestSmall, false)
       .value
@@ -29,7 +29,7 @@ class MultipartBench {
   }
 
   @Benchmark
-  def smallnewMultipart: Multipart[IO] = {
+  def smallNewMultipart: Multipart[IO] = {
     encoderNew
       .decode(requestSmall, false)
       .value
@@ -40,7 +40,29 @@ class MultipartBench {
   }
 
   @Benchmark
-  def bigoldMultipart: Multipart[IO] = {
+  def mediumOldMultipart: Multipart[IO] = {
+    encoderLive
+      .decode(requestMedium, false)
+      .value
+      .unsafeRunSync() match {
+      case Right(r) => r
+      case Left(l)  => throw l
+    }
+  }
+
+  @Benchmark
+  def mediumNewMultipart: Multipart[IO] = {
+    encoderNew
+      .decode(requestMedium, false)
+      .value
+      .unsafeRunSync() match {
+      case Right(r) => r
+      case Left(l)  => throw l
+    }
+  }
+
+  @Benchmark
+  def bigOldMultipart: Multipart[IO] = {
     encoderLive
       .decode(requestLarge, false)
       .value
@@ -51,9 +73,31 @@ class MultipartBench {
   }
 
   @Benchmark
-  def bignewMultipart: Multipart[IO] = {
+  def bigNewMultipart: Multipart[IO] = {
     encoderNew
       .decode(requestLarge, false)
+      .value
+      .unsafeRunSync() match {
+      case Right(r) => r
+      case Left(l)  => throw l
+    }
+  }
+
+  @Benchmark
+  def bigPartBodyOldMultipart: Multipart[IO] = {
+    encoderLive
+      .decode(requestHyooge, false)
+      .value
+      .unsafeRunSync() match {
+      case Right(r) => r
+      case Left(l)  => throw l
+    }
+  }
+
+  @Benchmark
+  def bigPartBodyNewMultipart: Multipart[IO] = {
+    encoderNew
+      .decode(requestHyooge, false)
       .value
       .unsafeRunSync() match {
       case Right(r) => r
@@ -66,26 +110,29 @@ class MultipartBench {
 object MultipartBench {
   val boundary = Boundary("----WebKitFormBoundarycaZFo8IAKVROTEeD")
 
+  val dumbBodySmall: Stream[IO, Byte] =
+    Stream
+      .emits(List.fill(10)("REEEEEEEEEEEEEEEEEE".getBytes()).flatMap(_.toList))
+      .covary[IO]
+
+  val dumbBodyMedium: Stream[IO, Byte] =
+    Stream
+      .emits(List.fill(50)("REEEEEEEEEEEEEEEEEE".getBytes()).flatMap(_.toList))
+      .covary[IO]
+
   val dumbBodyLarge: Stream[IO, Byte] =
     Stream
       .emits(
         List.fill(1000)("REEEEEEEEEEEEEEEEEE".getBytes()).flatMap(_.toList))
       .covary[IO]
 
-  val bodyLarge: Multipart[IO] = Multipart[IO](
-    Vector.fill(100)(
-      Part[IO](Headers(Header("Content-Type", "text/plain"),
-                       Header("Content-Type", "text/plain")),
-               dumbBodyLarge)),
-    Boundary("----WebKitFormBoundarycaZFo8IAKVROTEeD")
-  )
-
-  val dumbBodySmall: Stream[IO, Byte] =
+  val dumbBodyHyooge: Stream[IO, Byte] =
     Stream
-      .emits(List.fill(10)("REEEEEEEEEEEEEEEEEE".getBytes()).flatMap(_.toList))
+      .emits(
+        List.fill(100000)("REEEEEEEEEEEEEEEEEE".getBytes()).flatMap(_.toList))
       .covary[IO]
 
-  val bodySmall: Multipart[IO] = Multipart[IO](
+  val multipartSmall: Multipart[IO] = Multipart[IO](
     Vector.fill(10)(
       Part[IO](Headers(Header("Content-Type", "text/plain"),
                        Header("Content-Type", "text/plain")),
@@ -93,17 +140,69 @@ object MultipartBench {
     Boundary("----WebKitFormBoundarycaZFo8IAKVROTEeD")
   )
 
+  val multipartMedium: Multipart[IO] = Multipart[IO](
+    Vector.fill(50)(
+      Part[IO](Headers(Header("Content-Type", "text/plain"),
+                       Header("Content-Type", "text/plain")),
+               dumbBodyMedium)),
+    Boundary("----WebKitFormBoundarycaZFo8IAKVROTEeD")
+  )
+
+  val multipartLarge: Multipart[IO] = Multipart[IO](
+    Vector.fill(100)(
+      Part[IO](Headers(Header("Content-Type", "text/plain"),
+                       Header("Content-Type", "text/plain")),
+               dumbBodyLarge)),
+    Boundary("----WebKitFormBoundarycaZFo8IAKVROTEeD")
+  )
+
+  val multipartHyooge: Multipart[IO] = Multipart[IO](
+    Vector.fill(10)(
+      Part[IO](Headers(Header("Content-Type", "text/plain"),
+                       Header("Content-Type", "text/plain")),
+               dumbBodyHyooge)),
+    Boundary("----WebKitFormBoundarycaZFo8IAKVROTEeD")
+  )
+
   val byteStreamLarge: EntityBody[IO] =
-    EntityEncoder.multipartEncoder[IO].toEntity(bodyLarge).unsafeRunSync.body
+    EntityEncoder
+      .multipartEncoder[IO]
+      .toEntity(multipartLarge)
+      .unsafeRunSync
+      .body
 
   val byteStreamSmall: EntityBody[IO] =
-    EntityEncoder.multipartEncoder[IO].toEntity(bodySmall).unsafeRunSync.body
+    EntityEncoder
+      .multipartEncoder[IO]
+      .toEntity(multipartSmall)
+      .unsafeRunSync
+      .body
+
+  val byteStreamMedium: EntityBody[IO] =
+    EntityEncoder
+      .multipartEncoder[IO]
+      .toEntity(multipartMedium)
+      .unsafeRunSync
+      .body
+
+  val byteStreamHyooge: EntityBody[IO] =
+    EntityEncoder
+      .multipartEncoder[IO]
+      .toEntity(multipartHyooge)
+      .unsafeRunSync
+      .body
 
   val requestLarge: Request[IO] =
-    Request[IO](body = byteStreamLarge, headers = bodyLarge.headers)
+    Request[IO](body = byteStreamLarge, headers = multipartLarge.headers)
 
   val requestSmall: Request[IO] =
-    Request[IO](body = byteStreamSmall, headers = bodySmall.headers)
+    Request[IO](body = byteStreamSmall, headers = multipartSmall.headers)
+
+  val requestMedium: Request[IO] =
+    Request[IO](body = byteStreamMedium, headers = multipartMedium.headers)
+
+  val requestHyooge: Request[IO] =
+    Request[IO](body = byteStreamHyooge, headers = multipartHyooge.headers)
 
   val encoderLive: EntityDecoder[IO, Multipart[IO]] =
     EntityDecoder.multipart[IO]
